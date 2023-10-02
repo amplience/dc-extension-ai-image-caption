696a70ff350985c92f6f08b436e3d98ff1b8ced8
Chattox <33847899+Chattox@users.noreply.github.com>
2023-10-02 10:49:31 +0100

    fix: CMP-10496 revert error specific messaging to be more generic

--- src/components/StringField.tsx
+++ src/components/StringField.tsx
@@ -43,33 +43,7 @@ function StringField(props: StringFieldProps) {
   let invalid: boolean =
     isAboveMaxLength || isBelowMinLength || isNotMatchingPattern;
 
-  const errorMessages = {
-    invalidOrg: "Invalid organization",
-    permissionError: "Missing read permission",
-    invalidUrl: "Invalid image URL",
-    invalidHostName: "Invalid image hostname",
-    internalServerError:
-      "An error occurred processing your request, please try again",
-    quotaExceeded: "Exceeded allocated quota",
-  };
-
-  let captionErrorMsg = undefined;
-
-  if (captionError) {
-    if (captionError?.message.includes("Invalid organization")) {
-      captionErrorMsg = errorMessages.invalidOrg;
-    } else if (captionError?.message.includes("Missing permission")) {
-      captionErrorMsg = errorMessages.permissionError;
-    } else if (captionError?.message.includes("Invalid image URL")) {
-      captionErrorMsg = errorMessages.invalidUrl;
-    } else if (captionError?.message.includes("Invalid image hostname")) {
-      captionErrorMsg = errorMessages.invalidHostName;
-    } else if (captionError?.message.includes("Exceeded allocated quota")) {
-      captionErrorMsg = errorMessages.quotaExceeded;
-    } else {
-      captionErrorMsg = errorMessages.internalServerError;
-    }
-  }
+  const errorMessage = "An error occurred when processing your request";
 
   return (
     <div style={{ display: "flex", flexDirection: "column", marginLeft: 2 }}>
@@ -80,7 +54,7 @@ function StringField(props: StringFieldProps) {
         aria-label={description || label}
         value={value}
         error={captionError ? true : false}
-        helperText={captionErrorMsg}
+        helperText={captionError ? errorMessage : undefined}
         {...fieldProps}
       />
 
